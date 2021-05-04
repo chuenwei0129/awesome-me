eedbb7dda6886174b07c40c9a941c0369fc432e0
我眼里有碳酸 <chuenwei0129@gmail.com>
2021-05-05 00:59:43 +0800

    upd

---
+++ examples/eslint-test/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+    // 'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['prettier', 'react', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+    semi: ['error', 'never']
+  }
+}
---
+++ examples/eslint-test/test.js
@@ -0,0 +1,3 @@
+const a = '12'
+
+console.log(a)
