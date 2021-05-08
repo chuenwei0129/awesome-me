49ec57830753d460813845c24c2987a39c2fdba1
我眼里有碳酸 <chuenwei0129@gmail.com>
2021-05-08 18:13:12 +0800

    upd

--- examples/react/src/index.js
+++ examples/react/src/index.js
@@ -1,6 +1,8 @@
 import { createContext, memo, StrictMode, useContext, useState } from 'react'
 import ReactDOM from 'react-dom'
-const AppContext = createContext()
+// const AppContext = createContext()
+const ChildContext = createContext()
+const SonContext = createContext()
 
 function App() {
   console.log('app render')
@@ -8,11 +10,20 @@ function App() {
   const [sonNum, updateSonNum] = useState(0)
 
   return (
-    <AppContext.Provider value={{ childNum, sonNum }}>
-      <Father></Father>
-      <button onClick={() => updateChildNum(childNum + 1)}> + child </button>
-      <button onClick={() => updateSonNum(sonNum + 1)}> + son </button>
-    </AppContext.Provider>
+    // <AppContext.Provider value={{ childNum, sonNum }}>
+    //   <Father></Father>
+    //   <button onClick={() => updateChildNum(childNum + 1)}> + child </button>
+    //   <button onClick={() => updateSonNum(sonNum + 1)}> + son </button>
+    // </AppContext.Provider>
+
+    // 拆分 context
+    <ChildContext.Provider value={childNum}>
+      <SonContext.Provider value={sonNum}>
+        <Father></Father>
+        <button onClick={() => updateChildNum(childNum + 1)}> + child </button>
+        <button onClick={() => updateSonNum(sonNum + 1)}> + son </button>
+      </SonContext.Provider>
+    </ChildContext.Provider>
   )
 }
 
@@ -23,19 +34,19 @@ const Father = memo(() => {
 
 function Child() {
   console.log('child render')
-  const { childNum } = useContext(AppContext)
+  const childNum = useContext(ChildContext)
   return <h1>child: {childNum}</h1>
 }
 
 function Son() {
   console.log('son render')
-  const { sonNum } = useContext(AppContext)
+  const sonNum = useContext(SonContext)
   return <h1>son: {sonNum}</h1>
 }
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <App></App>
   </StrictMode>,
   document.getElementById('root')
 )
