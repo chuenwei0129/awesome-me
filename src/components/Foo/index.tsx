66e4dd4d872a282ddaba60ac4b679d24c46c430b
chuenwei0129 <chuenwei0129@gmail.com>
2024-06-14 01:44:02 +0800

    feat: 测试组件发布

--- src/components/Foo/index.tsx
+++ src/components/Foo/index.tsx
@@ -1,5 +1,6 @@
 import React, { type FC } from 'react';
 import styled from 'styled-components';
+import './styles.scss';
 
 const Button = styled.button`
   background-color: #fff;
@@ -15,6 +16,11 @@ const Button = styled.button`
   }
 `;
 
-const Foo: FC<{ title: string }> = (props) => <Button>{props.title}</Button>;
+const Foo: FC<{ title: string }> = (props) => (
+  <Button>
+    <span>{props.title}</span>
+    <span className={'helloWorld'}>hello world</span>
+  </Button>
+);
 
 export default Foo;
