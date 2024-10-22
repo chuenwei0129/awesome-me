e580a9485099c69ba4825a3598a04d7bf18b4769
chuenwei0129 <chuenwei0129@gmail.com>
2024-10-22 20:37:56 +0800

    Updates

--- Space__demos.b2db4e1c.async.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self.webpackChunknaifu=self.webpackChunknaifu||[]).push([[206],{52467:function(r,s,e){e.r(s);var o=e(68400),t=e.n(o),a=e(76508),u=e(67294),m=e(35480),_=e(85893),l,n=m.default.div(l||(l=t()([`
-  width: 100px;
-  height: 100px;
-  background: pink;
-  border: 1px solid #000;
-`]))),d=function(){return(0,_.jsx)(_.Fragment,{children:(0,_.jsx)(a.ConfigProvider,{space:{size:100},children:(0,_.jsxs)(a.Space,{split:(0,_.jsx)(n,{style:{background:"yellow"}}),children:[(0,_.jsx)(n,{}),(0,_.jsx)(n,{})]})})})};s.default=d}}]);
