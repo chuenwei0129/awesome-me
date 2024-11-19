71e1bb259389515eae1fbb62f630058ac22d797e
chuenwei0129 <chuenwei0129@users.noreply.github.com>
2024-11-19 03:43:26 +0000

    deploy: 4b22b44588921c7ac37219aa7313a0acb12d5676

---
+++ meta__hooks.fb6a0847.async.js
@@ -0,0 +1,215 @@
+"use strict";(self.webpackChunknaifu=self.webpackChunknaifu||[]).push([[3518],{81123:function(d,e,n){var s;n.r(e),n.d(e,{demos:function(){return y}});var l=n(15009),m=n.n(l),I=n(99289),f=n.n(I),o=n(67294),P=n(52175),v=n(54701),y={"src-hooks-use-latest-demo-demo1":{component:o.memo(o.lazy(function(){return Promise.all([n.e(3550),n.e(8500),n.e(4976),n.e(8772),n.e(4701),n.e(9950),n.e(2433)]).then(n.bind(n,63866))})),asset:{type:"BLOCK",id:"src-hooks-use-latest-demo-demo1",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(90349).Z},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{react:s||(s=n.t(o,2))},renderOpts:{compile:function(){var p=f()(m()().mark(function c(){var a,u=arguments;return m()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,Promise.all([n.e(4976),n.e(1011)]).then(n.bind(n,37335));case 2:return t.abrupt("return",(a=t.sent).default.apply(a,u));case 3:case"end":return t.stop()}},c)}));function i(){return p.apply(this,arguments)}return i}()}},"src-hooks-use-latest-demo-demo2":{component:o.memo(o.lazy(function(){return Promise.all([n.e(3550),n.e(8500),n.e(4976),n.e(8772),n.e(4701),n.e(9950),n.e(2433)]).then(n.bind(n,11560))})),asset:{type:"BLOCK",id:"src-hooks-use-latest-demo-demo2",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(53460).Z},react:{type:"NPM",value:"18.3.1"},naifu:{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:s||(s=n.t(o,2)),naifu:v},renderOpts:{compile:function(){var p=f()(m()().mark(function c(){var a,u=arguments;return m()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,Promise.all([n.e(4976),n.e(1011)]).then(n.bind(n,37335));case 2:return t.abrupt("return",(a=t.sent).default.apply(a,u));case 3:case"end":return t.stop()}},c)}));function i(){return p.apply(this,arguments)}return i}()}}}},76:function(d,e,n){var s;n.r(e),n.d(e,{demos:function(){return p}});var l=n(15009),m=n.n(l),I=n(99289),f=n.n(I),o=n(67294),P=n(19020),v=n(13008),y=n(54701),p={"src-hooks-use-memoized-fn-demo-demo1":{component:o.memo(o.lazy(function(){return Promise.all([n.e(3550),n.e(8500),n.e(4976),n.e(8772),n.e(4701),n.e(9950),n.e(2433)]).then(n.bind(n,8214))})),asset:{type:"BLOCK",id:"src-hooks-use-memoized-fn-demo-demo1",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(28682).Z},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.22.1"},naifu:{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:s||(s=n.t(o,2)),antd:v,naifu:y},renderOpts:{compile:function(){var i=f()(m()().mark(function a(){var u,h=arguments;return m()().wrap(function(r){for(;;)switch(r.prev=r.next){case 0:return r.next=2,Promise.all([n.e(4976),n.e(1011)]).then(n.bind(n,37335));case 2:return r.abrupt("return",(u=r.sent).default.apply(u,h));case 3:case"end":return r.stop()}},a)}));function c(){return i.apply(this,arguments)}return c}()}},"src-hooks-use-memoized-fn-demo-demo2":{component:o.memo(o.lazy(function(){return Promise.all([n.e(3550),n.e(8500),n.e(4976),n.e(8772),n.e(4701),n.e(9950),n.e(2433)]).then(n.bind(n,51475))})),asset:{type:"BLOCK",id:"src-hooks-use-memoized-fn-demo-demo2",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(88158).Z},react:{type:"NPM",value:"18.3.1"},naifu:{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:s||(s=n.t(o,2)),naifu:y},renderOpts:{compile:function(){var i=f()(m()().mark(function a(){var u,h=arguments;return m()().wrap(function(r){for(;;)switch(r.prev=r.next){case 0:return r.next=2,Promise.all([n.e(4976),n.e(1011)]).then(n.bind(n,37335));case 2:return r.abrupt("return",(u=r.sent).default.apply(u,h));case 3:case"end":return r.stop()}},a)}));function c(){return i.apply(this,arguments)}return c}()}}}},86760:function(d,e,n){var s;n.r(e),n.d(e,{demos:function(){return y}});var l=n(15009),m=n.n(l),I=n(99289),f=n.n(I),o=n(67294),P=n(35862),v=n(54701),y={"src-hooks-use-set-state-demo-demo1":{component:o.memo(o.lazy(function(){return Promise.all([n.e(3550),n.e(8500),n.e(4976),n.e(8772),n.e(4701),n.e(9950),n.e(2433)]).then(n.bind(n,98372))})),asset:{type:"BLOCK",id:"src-hooks-use-set-state-demo-demo1",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(17663).Z},react:{type:"NPM",value:"18.3.1"},naifu:{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:s||(s=n.t(o,2)),naifu:v},renderOpts:{compile:function(){var p=f()(m()().mark(function c(){var a,u=arguments;return m()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,Promise.all([n.e(4976),n.e(1011)]).then(n.bind(n,37335));case 2:return t.abrupt("return",(a=t.sent).default.apply(a,u));case 3:case"end":return t.stop()}},c)}));function i(){return p.apply(this,arguments)}return i}()}},"src-hooks-use-set-state-demo-demo2":{component:o.memo(o.lazy(function(){return Promise.all([n.e(3550),n.e(8500),n.e(4976),n.e(8772),n.e(4701),n.e(9950),n.e(2433)]).then(n.bind(n,51760))})),asset:{type:"BLOCK",id:"src-hooks-use-set-state-demo-demo2",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(75806).Z},react:{type:"NPM",value:"18.3.1"},naifu:{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:s||(s=n.t(o,2)),naifu:v},renderOpts:{compile:function(){var p=f()(m()().mark(function c(){var a,u=arguments;return m()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,Promise.all([n.e(4976),n.e(1011)]).then(n.bind(n,37335));case 2:return t.abrupt("return",(a=t.sent).default.apply(a,u));case 3:case"end":return t.stop()}},c)}));function i(){return p.apply(this,arguments)}return i}()}}}},75661:function(d,e,n){n.r(e),n.d(e,{texts:function(){return l}});var s=n(52175);const l=[{value:"\u83B7\u53D6\u7ED9\u5B9A\u53D8\u91CF\u7684\u6700\u65B0\u503C\u3002",paraId:0,tocIndex:0},{value:"\u6709\u5982\u4E0B\u573A\u666F\uFF0C\u5F53\u4F60\u6309\u4E0B \u201C\u53D1\u9001\u201D \u540E\uFF0C\u63D0\u793A\u6846\u4F1A\u5728 3000 ms \u540E\u5EF6\u8FDF\u663E\u793A\u3002",paraId:1,tocIndex:1},{value:"\u8F93\u5165 \u201C123\u201D\uFF0C\u6309\u4E0B\u53D1\u9001\uFF0C\u7136\u540E\u518D\u6B21\u5FEB\u901F\u7F16\u8F91\u8F93\u5165 \u201C456\u201D\uFF0C\u63D0\u793A\u6846\u4F1A\u663E\u793A \u201C123\u201D\u3002",paraId:2,tocIndex:1},{value:"\u63D0\u793A\u6846\u4F1A\u663E\u793A\u70B9\u51FB\u65F6\u7684\u5185\u5BB9\u800C\u975E\u5F53\u524D\u8F93\u5165\u6587\u672C\uFF0C\u5373 React \u4F1A\u4FDD\u7559\u6BCF\u6B21\u6E32\u67D3\u770B\u5230\u7684\u90A3\u4E2A text\u3002",paraId:3},{value:"\u5176\u5B9E React \u662F\u5728 Hooks API \u65F6\u6709\u610F\u628A\u884C\u4E3A\u6539\u6210\u4E86\u73B0\u5728\u8FD9\u6837\u3002React Class API \u7684\u884C\u4E3A\u548C Vue \u90FD\u4F1A\u62FF\u5230\u6700\u65B0\u7684 text\u3002",paraId:4},{value:"\u4E3A\u4EC0\u4E48 React \u8981\u8FD9\u6837\u8BBE\u8BA1 API\uFF1F",paraId:5},{value:"Vue \u548C React \u7684\u8FD9\u4E2A\u884C\u4E3A\u5404\u662F\u51FA\u4E8E\u4EC0\u4E48\u8003\u8651\uFF1F",paraId:6},{value:"\u5F53\u6211\u4EEC\u9700\u8981\u63D0\u793A\u6846\u663E\u793A\u5F53\u524D\u6587\u672C\u800C\u975E\u70B9\u51FB\u65F6\u7684\u5185\u5BB9\u65F6\uFF0C\u5C31\u662F\u6211\u4EEC\u7684 ",paraId:7},{value:"useLatest",paraId:7},{value:" \u51FA\u5C71\u7684\u65F6\u523B\u4E86\uFF0C\u5176\u5B9E\u5C31\u662F\u4E2A ref\u3002",paraId:7}]},94643:function(d,e,n){n.r(e),n.d(e,{texts:function(){return l}});var s=n(19020);const l=[{value:"\u6301\u4E45\u5316 function \u7684 Hook\uFF0C\u7406\u8BBA\u4E0A\uFF0C\u53EF\u4EE5\u4F7F\u7528 useMemoizedFn \u5B8C\u5168\u4EE3\u66FF useCallback\u3002",paraId:0,tocIndex:0},{value:"\u5B9E\u73B0\u539F\u7406\u662F\u901A\u8FC7 useRef \u4FDD\u6301 function \u5F15\u7528\u5730\u5740\u4E0D\u53D8\uFF0C\u5E76\u4E14\u6BCF\u6B21\u6267\u884C\u90FD\u53EF\u4EE5\u62FF\u5230\u6700\u65B0\u7684 state \u503C\u3002",paraId:1,tocIndex:0},{value:"\u5728\u67D0\u4E9B\u573A\u666F\u4E2D\uFF0C\u6211\u4EEC\u9700\u8981\u4F7F\u7528 useCallback \u6765\u8BB0\u4F4F\u4E00\u4E2A\u51FD\u6570\uFF0C\u4F46\u662F\u5728\u7B2C\u4E8C\u4E2A\u53C2\u6570 deps \u53D8\u5316\u65F6\uFF0C\u4F1A\u91CD\u65B0\u751F\u6210\u51FD\u6570\uFF0C\u5BFC\u81F4\u51FD\u6570\u5730\u5740\u53D8\u5316\u3002",paraId:2,tocIndex:1},{value:"\u4F7F\u7528 useMemoizedFn\uFF0C\u53EF\u4EE5\u7701\u7565\u7B2C\u4E8C\u4E2A\u53C2\u6570 deps\uFF0C\u540C\u65F6\u4FDD\u8BC1\u51FD\u6570\u5730\u5740\u6C38\u8FDC\u4E0D\u4F1A\u53D8\u5316\u3002",paraId:3,tocIndex:1},{value:"useMemoizedFn \u8FD4\u56DE\u7684\u51FD\u6570\u4E0E\u4F20\u5165\u7684 fn \u7684\u5F15\u7528\u5B8C\u5168\u4E0D\u540C\uFF08\u56E0\u4E3A\u8FD4\u56DE\u7684\u662F\u4E2A\u5BF9 fn \u5305\u4E86\u4E00\u5C42\u7684\u9AD8\u9636\u51FD\u6570\uFF09\uFF0C\u4E14\u6CA1\u6709\u7EE7\u627F fn \u81EA\u8EAB\u7684\u5C5E\u6027\u3002\u5982\u679C\u60F3\u8981\u6301\u4E45\u5316\u540E\u51FD\u6570\u81EA\u8EAB\u7684\u5C5E\u6027\u4E0D\u4E22\u5931\uFF0C\u76EE\u524D useMemoizedFn \u6EE1\u8DB3\u4E0D\u4E86\uFF0C\u8BF7\u964D\u7EA7\u4F7F\u7528 useCallback\u3001useMemo\u3002",paraId:4,tocIndex:2}]},64070:function(d,e,n){n.r(e),n.d(e,{texts:function(){return l}});var s=n(35862);const l=[{value:"\u7528\u4E8E\u4EE5\u5BF9\u8C61\u5F62\u5F0F\u7BA1\u7406\u72B6\u6001\uFF0C\u652F\u6301\u90E8\u5206\u66F4\u65B0\u548C\u5408\u5E76\u3002",paraId:0,tocIndex:0},{value:"\u6211\u4EEC\u77E5\u9053\uFF0CReact \u4E2D\u7684 State \u662F\u4E0D\u53EF\u53D8\u6570\u636E\u3002\u6240\u4EE5\u6211\u4EEC\u7ECF\u5E38\u9700\u8981\u5199\u7C7B\u4F3C\u5982\u4E0B\u7684\u4EE3\u7801\uFF1A",paraId:1,tocIndex:1},{value:`setState(prev => ({
+  ...prev,
+  name: 'Chu',
+  others: {
+    ...prev.others,
+    age: '27',
+  },
+}));
+`,paraId:2,tocIndex:1},{value:"\u901A\u8FC7 useSetState\uFF0C\u53EF\u4EE5\u7701\u53BB\u5BF9\u8C61\u6269\u5C55\u8FD0\u7B97\u7B26\u64CD\u4F5C\u8FD9\u4E2A\u6B65\u9AA4\uFF0C\u5373\uFF1A",paraId:3,tocIndex:1},{value:`setState(prev => ({
+  name: 'chu',
+  others: {
+    age: '27',
+  },
+}));
+`,paraId:4,tocIndex:1},{value:"\u5176\u672C\u8D28\u662F\u5229\u7528\u9AD8\u9636\u51FD\u6570\u5728\u8C03\u7528 setState \u4E4B\u524D\u505A\u4E00\u4E9B\u989D\u5916\u7684\u5904\u7406\uFF0C\u5373\u5C06\u5BF9\u8C61\u62D3\u5C55\u8FD0\u7B97\u7B26\u7684\u64CD\u4F5C\u5C01\u88C5\u5230\u5185\u90E8\u3002",paraId:5,tocIndex:1},{value:"\u6240\u4EE5\u624D\u53EB useSetState\u3002",paraId:6,tocIndex:1},{value:"\u5F53\u7136\uFF0C\u5199\u4E1A\u52A1\u65F6\uFF0C\u4E00\u822C\u4F1A\u7528 ",paraId:7,tocIndex:1},{value:"useImmer",paraId:7,tocIndex:1},{value:" \u6765\u89E3\u51B3\u7C7B\u4F3C\u95EE\u9898\u3002",paraId:7,tocIndex:1},{value:"\u5728\u8FD9\u4E2A\u4F8B\u5B50\u4E2D\uFF0C\u6211\u4EEC\u5C55\u793A\u4E86\u5982\u4F55\u53EA\u66F4\u65B0\u72B6\u6001\u5BF9\u8C61\u4E2D\u7684\u90E8\u5206\u5185\u5BB9\u3002",paraId:8,tocIndex:3},{value:"\u5047\u8BBE\u4F60\u9700\u8981\u8BB0\u5F55\u805A\u4F1A\u9080\u8BF7\u7684\u5609\u5BBE\u4EBA\u6570\u5E76\u52A8\u6001\u66F4\u65B0\u3002\u5728\u8FD9\u4E2A\u4F8B\u5B50\u4E2D\uFF0C\u6211\u4EEC\u5C55\u793A\u4E86\u5982\u4F55\u57FA\u4E8E\u524D\u4E00\u4E2A\u72B6\u6001\u6765\u66F4\u65B0\u5F53\u524D\u72B6\u6001\u3002",paraId:9,tocIndex:4}]},90349:function(d,e){e.Z=`import React, { useState } from 'react';
+import type { ChangeEvent } from 'react';
+
+export default function Chat() {
+  const [text, setText] = useState('');
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setText(e.target.value);
+  }
+
+  function handleSend() {
+    setTimeout(() => {
+      alert('\u6B63\u5728\u53D1\u9001\uFF1A' + text);
+    }, 3000);
+  }
+
+  return (
+    <>
+      <input value={text} onChange={handleChange} />{' '}
+      <button type="button" onClick={handleSend}>
+        \u53D1\u9001
+      </button>
+    </>
+  );
+}
+`},53460:function(d,e){e.Z=`import React, { useState } from 'react';
+import type { ChangeEvent } from 'react';
+import { useLatest } from 'naifu'
+
+export default function Chat() {
+  const [text, setText] = useState('');
+  const latestText = useLatest(text)
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setText(e.target.value);
+  }
+
+  function handleSend() {
+    setTimeout(() => {
+      alert('\u6B63\u5728\u53D1\u9001\uFF1A' + latestText.current);
+    }, 3000);
+  }
+
+  return (
+    <>
+      <input value={text} onChange={handleChange} />{' '}
+      <button type="button" onClick={handleSend}>
+        \u53D1\u9001
+      </button>
+    </>
+  );
+}
+`},28682:function(d,e){e.Z=`import React, { useState, useCallback, memo } from 'react';
+import { message } from 'antd';
+import { useMemoizedFn } from 'naifu';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Child = memo(({ f, name }: { f: () => void, name: string }) => {
+  return <div>
+    <h2>{name} \u4F5C\u4E3A props \u4F20\u9012\u7ED9\u5B50\u7EC4\u4EF6</h2>
+    <p>\u662F\u5426\u91CD\u6E32\u67D3\uFF1A{Math.random()}</p>
+  </div>
+})
+
+export default () => {
+  const [count, setCount] = useState(0);
+
+  const callbackFn = useCallback(() => {
+    message.info(\`Current count is \${count}\`);
+  }, [count]);
+
+  const memoizedFn = useMemoizedFn(() => {
+    message.info(\`Current count is \${count}\`);
+  });
+
+  return (
+    <>
+      <p>count: {count}</p>
+      <button
+        type="button"
+        onClick={() => {
+          setCount((c) => c + 1);
+        }}
+      >
+        Add Count
+      </button>
+      <div style={{ marginTop: 16 }}>
+        <button type="button" onClick={callbackFn}>
+          call callbackFn
+        </button>
+        <button type="button" onClick={memoizedFn} style={{ marginLeft: 8 }}>
+          call memoizedFn
+        </button>
+      </div>
+      <Child f={callbackFn} name='callbackFn'></Child>
+      <Child f={memoizedFn} name='memoizedFn'></Child>
+    </>
+  );
+};
+`},88158:function(d,e){e.Z=`import React, { useState, useCallback } from 'react';
+import { PickFunction } from '../../../types/shared';
+import { useMemoizedFn } from 'naifu'
+
+// \u5B9A\u4E49\u5E26\u6709\u81EA\u5B9A\u4E49\u5C5E\u6027\u7684\u51FD\u6570\u7C7B\u578B
+interface IncrementFunction extends PickFunction<(...args: any[]) => any> {
+  (): void;
+  customProperty?: string;
+}
+
+const WithAttributes: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+
+  // \u5B9A\u4E49\u6709\u81EA\u5B9A\u4E49\u5C5E\u6027\u7684\u51FD\u6570
+  const increment: IncrementFunction = useCallback(() => {
+    setCount((prevCount) => prevCount + 1);
+  }, []);
+
+  // \u6DFB\u52A0\u81EA\u5B9A\u4E49\u5C5E\u6027
+  // eslint-disable-next-line react-compiler/react-compiler
+  increment.customProperty = 'I am a custom property';
+
+  // \u5C1D\u8BD5\u4F7F\u7528 useMemoizedFn \u6301\u4E45\u5316\u51FD\u6570
+  const memoizedIncrement = useMemoizedFn(increment);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={memoizedIncrement}>Increment</button>
+
+      {/* \u663E\u793A\u81EA\u5B9A\u4E49\u5C5E\u6027 */}
+      <p>Original custom property: {increment.customProperty}</p>
+      <p>
+        Memoized function custom property: {memoizedIncrement.customProperty ? memoizedIncrement.customProperty : 'undefined'}
+      </p>
+    </div>
+  );
+};
+
+export default WithAttributes;
+`},17663:function(d,e){e.Z=`import React from 'react';
+import { useSetState } from 'naifu';
+
+interface ShoppingList {
+  drinks: string;
+  snacks: string;
+  [key: string]: any;
+}
+
+const PartyPrep = () => {
+  const [shoppingList, setShoppingList] = useSetState<ShoppingList>({
+    drinks: '',
+    snacks: '',
+  });
+
+  return (
+    <div>
+      <pre>{JSON.stringify(shoppingList, null, 2)}</pre>
+      <p>
+        <button type="button" onClick={() => setShoppingList({ drinks: 'Coke' })}>
+          Add Drinks
+        </button>
+        <button type="button" onClick={() => setShoppingList({ snacks: 'Chips' })} style={{ margin: '0 8px' }}>
+          Add Snacks
+        </button>
+        <button type="button" onClick={() => setShoppingList({ fruits: 'Apple' })}>
+          Add Fruits
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default PartyPrep;
+`},75806:function(d,e){e.Z=`import React from 'react';
+import { useSetState } from 'naifu';
+
+interface PartyState {
+  guestCount: number;
+  partyTheme: string;
+}
+
+const GuestList = () => {
+  const [partyState, setPartyState] = useSetState<PartyState>({
+    guestCount: 0,
+    partyTheme: 'Beach Party',
+  });
+
+  return (
+    <div>
+      <pre>{JSON.stringify(partyState, null, 2)}</pre>
+      <p>
+        <button type="button" onClick={() => setPartyState((prev) => ({ guestCount: prev.guestCount + 1 }))}>
+          Add Guest
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default GuestList;
+`}}]);
