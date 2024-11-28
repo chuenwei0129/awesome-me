9193ffddc6ab672b2e6c78e82963c7da734ee722
chuenwei0129 <chuenwei0129@gmail.com>
2024-11-28 14:38:51 +0800

    build(deps): 移除 react-dnd-mouse-backend 依赖
    - 从 package.json 中删除了 react-dnd-mouse-backend 依赖
    - 从 pnpm-lock.yaml 中移除了相关的依赖项
    - 在 .eslintrc.js 中添加了 React 编译器相关的 ESLint 规则
    - 新增了 Ant Design X 相关的文档文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: require.resolve('@umijs/lint/dist/config/eslint'),
   plugins: ['eslint-plugin-react-compiler'],
   rules: {
+    // https://zh-hans.react.dev/blog/2024/10/21/react-compiler-beta-release
     'react-compiler/react-compiler': 'error',
     'react/button-has-type': 'off',
   },
---
+++ docs/playground/antd-x-demo.md
@@ -0,0 +1,10 @@
+---
+group:
+  title: React
+  order: 0
+title: Ant Design X
+toc: content
+order: -1003
+---
+
+> TODO
