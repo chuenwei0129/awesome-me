1aebe1bbd4ef17506c0b3f5d117983b05bb6af22
chuenwei0129 <chuenwei0129@gmail.com>
2025-04-17 22:25:24 +0800

    docs(playground): 添加类型挑战第 00 题
    - 生成一个长度为 5 的数组：[0, 0, 0, 0, 0]
    - 实现了一个递归类型 Loop 来生成指定长度的数组
    - 添加了 State 类型和 Pipeline 接口作为扩展内容

---
+++ docs/playground/_type-challenges/00.ts
@@ -0,0 +1,19 @@
+// 生成一个长度为5的数组：`[0, 0, 0, 0, 0]`
+
+type Result = Loop<5>; // expected to be [0, 0, 0, 0, 0]
+
+// 解答
+type Loop<N extends number, R extends any[] = []> = R['length'] extends N ? R : Loop<N, [...R, 0]>;
+
+type State = 'initialized' | 'processing' | 'succeeded' | 'failed';
+
+interface Pipeline<T extends State> {
+  state: T;
+  start(this: Pipeline<'initialized'>): Pipeline<'processing'>;
+  finish(this: Pipeline<'processing'>): Pipeline<'succeeded'>;
+  retry(this: Pipeline<'failed'>): Pipeline<'processing'>;
+}
+
+declare const pipeline: Pipeline<'initialized'>;
+
+pipeline.start();
