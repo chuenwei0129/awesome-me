395784d614e433d8b9a78975fb26a7e74263f85e
chuenwei0129 <chuenwei0129@gmail.com>
2025-07-27 09:13:12 +0800

    feat: 更新文档配置和新增 logo
    - 在 .dumirc.ts 中添加输出路径、文档起始路由、静态资源路径、logo 和社交链接配置
    - 新增 public/logo.svg 文件，提供网站 logo 图标

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  // 配置输出路径
   outputPath: 'docs-dist',
+  // 配置文档起始路由和静态资源起始路径
   base: '/awesome-me/', // 文档起始路由
   publicPath: '/awesome-me/', // 静态资源起始路径
+  // 配置 logo 和 favicons
+  logo: '/awesome-me/original.svg',
+  favicons: ['/awesome-me/original.svg'],
+  // 配置文档名称
   themeConfig: {
-    name: 'awesome-me',
+    // 配置网站名称
+    name: 'HogLog',
+    // 配置社交链接
+    socialLinks: {
+      github: 'https://github.com/chuenwei0129/awesome-me',
+    },
   },
+  // 使用 tailwindcss
   extraPostCSSPlugins: [require('@tailwindcss/postcss')],
 });
