1fbd0827e67709572731c86a933df284d6664320
chuenwei0129 <chuenwei0129@gmail.com>
2025-07-27 09:13:00 +0800

    feat: 添加 GitHub Pages 部署配置和更新文档
    - 在 .dumirc.ts 中新增文档起始路由和静态资源路径配置
    - 在 package.json 中添加 gh-pages 依赖和部署脚本
    - 新增 GitHub Actions 工作流配置文件以自动部署文档
    - 更新文档示例代码块格式为 bash，提升可读性

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: '/awesome-me/', // 文档起始路由
+  publicPath: '/awesome-me/', // 静态资源起始路径
   themeConfig: {
     name: 'awesome-me',
   },
