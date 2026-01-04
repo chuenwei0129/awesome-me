69765d059640854fd6e097ef861ac39184a7c7ed
chuenwei0129 <chuenwei0129@gmail.com>
2026-01-05 00:14:52 +0800

    feat(.dumirc.ts): 引入tailwindcss插件配置
    添加tailwindcss插件支持，包括相关依赖项和配置项，
    为项目提供现代化的CSS框架支持

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,4 +5,8 @@ export default defineConfig({
   themeConfig: {
     name: '@c6i',
   },
+  // 引入 tailwindcss 插件
+  plugins: ['@umijs/plugins/dist/tailwindcss'],
+  tailwindcss: {},
+  autoprefixer: {},
 });
